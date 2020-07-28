Fix some mistakes in MPC implementation, optimal health calculation, and final objective.

examples/ex_quadratic_mpc.py
@@ -42,7 +42,7 @@ def main(figpath = "", datapath = ""):
 
 	# Actual health status transition function.
 	mu = 0
-	sigma = 0.025
+	sigma = 0.05
 	h_noise = mu + sigma*np.random.randn(T,K)
 	# health_map = lambda h,t: h
 	# health_map = lambda h,t: h + h_noise[t]
@@ -92,18 +92,19 @@ def main(figpath = "", datapath = ""):
 	# Health constraints.
 	health_lower = np.full((T,K), -np.inf)
 	health_upper = np.full((T,K), np.inf)
-	# health_lower[:,1] = -20     # Lower bound on OARs.
-	# health_lower[:,2] = -10
-	# health_lower[:,3] = -10
-	# health_lower[:,4] = -30
-	# health_upper[:15,0] = 25    # Upper bound on PTV for t = 1,...,15.
-	# health_upper[15:,0] = 5     # Upper bound on PTV for t = 16,...,20.
+	health_lower[:,1] = -1.0     # Lower bound on OARs.
+	health_lower[:,2] = -2.0
+	health_lower[:,3] = -2.0
+	health_lower[:,4] = -3.0
+	health_upper[:15,0] = 2.0    # Upper bound on PTV for t = 1,...,15.
+	health_upper[15:,0] = 0.05   # Upper bound on PTV for t = 16,...,20.
 	patient_rx["health_constrs"] = {"lower": health_lower[:,~is_target], "upper": health_upper[:,is_target]}
 
 	# Dynamic treatment.
 	d_init = np.zeros((T, K))
 	# res_dynamic = dyn_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, solver = "MOSEK")
-	res_dynamic = dyn_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, health_map = health_map, d_init = d_init, use_slack = True, max_iter = 15, solver = "MOSEK", ccp_verbose = True)
+	res_dynamic = dyn_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, health_map = health_map, d_init = d_init,
+								 slack_weight = 1e4, use_slack = True, max_iter = 15, solver = "MOSEK", ccp_verbose = True)
 	# res_dynamic = dyn_quad_treat_admm(A_list, alpha, beta, gamma, h_init, patient_rx, health_map = health_map, rho = 1, max_iter = 1000, solver = "MOSEK", admm_verbose = True)
 	print("Dynamic Treatment")
 	print("Status:", res_dynamic["status"])
@@ -129,7 +130,8 @@ def main(figpath = "", datapath = ""):
 	# Dynamic treatment with MPC.
 	print("\nStarting MPC algorithm...")
 	# res_mpc = mpc_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, health_map = health_map, solver = "MOSEK", mpc_verbose = True)
-	res_mpc = mpc_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, health_map = health_map, d_init = d_init, use_ccp_slack = True, max_iter = 15, solver = "MOSEK", mpc_verbose = True)
+	res_mpc = mpc_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, health_map = health_map, d_init = d_init,
+							 ccp_slack_weight = 1e4, use_ccp_slack = True, max_iter = 15, solver = "MOSEK", mpc_verbose = True)
 	# res_mpc = mpc_quad_treat_admm(A_list, alpha, beta, gamma, h_init, patient_rx, health_map = health_map, rho = 1, max_iter = 1000, solver = "MOSEK", mpc_verbose = True)
 	print("\nMPC Treatment")
 	print("Status:", res_mpc["status"])

fractionation/quad_funcs.py
@@ -42,19 +42,24 @@ def dyn_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, T_recov = 0, 
 	
 	# Construct full results.
 	beams_all = pad_matrix(b.value, T_recov)
-	health_all = pad_matrix(h.value, T_recov)
 	doses_all = pad_matrix(d.value, T_recov)
 	doses_parms = pad_matrix(d_parm.value, T_recov)
 	alpha_pad = np.vstack([alpha, np.zeros((T_recov,K))])
 	beta_pad  = np.vstack([beta, np.zeros((T_recov,K))])
 
-	health_est = health_prog_est(h_init, T_treat + T_recov, alpha_pad, beta_pad, gamma, doses_all, doses_parms, patient_rx["is_target"], health_map)
+	# Extend optimal health status into recovery stage using linear-quadratic model.
+	health_recov = health_prog_act_range(h.value[-1], T_treat - 1, T_treat + T_recov, gamma = gamma,
+										 is_target = patient_rx["is_target"], health_map = health_map)
+	health_opt_recov = np.row_stack([h.value, health_recov[1:]])
+
+	# Compute health status from optimal doses using linearized/linear-quadratic models.
 	health_proj = health_prog_act(h_init, T_treat + T_recov, alpha_pad, beta_pad, gamma, doses_all, patient_rx["is_target"], health_map)
+	health_est = health_prog_est(h_init, T_treat + T_recov, alpha_pad, beta_pad, gamma, doses_all, doses_parms, patient_rx["is_target"], health_map)
 	obj = dyn_quad_obj(d, health_proj[:(T_treat+1)], patient_rx).value
 	if use_slack:
 		obj += slack_weight*np.sum(h_dyn_slack.value)
 	return {"obj": obj, "status": result["status"], "solve_time": result["solve_time"], "num_iters": result["num_iters"],
-			"beams": beams_all, "doses": doses_all, "health": health_proj, "health_opt": health_all, "health_est": health_est,
+			"beams": beams_all, "doses": doses_all, "health": health_proj, "health_opt": health_opt_recov, "health_est": health_est,
 			"health_slack": result["health_slack"]}
 
 def mpc_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, T_recov = 0, health_map = lambda h,t: h, d_init = None,
@@ -67,10 +72,14 @@ def mpc_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, T_recov = 0, 
 	# Initialize values.
 	beams = np.zeros((T_treat,n))
 	doses = np.zeros((T_treat,K))
+	parms = np.zeros((T_treat,K))
+	dyn_slacks = np.zeros((T_treat,K))
+	health_opt = np.zeros((T_treat + 1,K))
+	health_opt[0] = h_init
+
 	num_iters = 0
 	solve_time = 0
 	status_list = []
-	
 	h_cur = h_init
 	for t_s in range(T_treat):
 		# Drop prescription for previous periods.
@@ -96,7 +105,8 @@ def mpc_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, T_recov = 0, 
 			print("\nSolver failed with status {0}. Retrying with slack enabled...".format(status))
 
 			prob, b, h, d, d_parm, h_dyn_slack, s_vars = build_dyn_slack_quad_prob(T_left*[A_list[t_s]], np.row_stack(T_left*[alpha[t_s]]), np.row_stack(T_left*[beta[t_s]]),
-																	  np.row_stack(T_left*[gamma[t_s]]), h_cur, rx_cur, T_recov, mpc_slack_weights, mpc_slack_final)
+																	np.row_stack(T_left*[gamma[t_s]]), h_cur, rx_cur, T_recov, use_ccp_slack, ccp_slack_weight,
+																	mpc_slack_weights, mpc_slack_final)
 			result = ccp_solve(prob, d, d_parm, d_init, h_dyn_slack, *args, **kwargs)
 			status = result["status"]
 			if status not in cvxpy_s.SOLUTION_PRESENT:
@@ -113,30 +123,37 @@ def mpc_quad_treat(A_list, alpha, beta, gamma, h_init, patient_rx, T_recov = 0, 
 		solve_time += result["solve_time"]
 		status_list.append(status)
 
-		# Save beams and doses for current period.
-		# TODO: Save optimal health, health dynamics slack, and dose parameter values.
+		# Save beams, doses, and health statuses for current period.
 		beams[t_s] = b.value[0]
 		doses[t_s] = d.value[0]
-		d_init = d.value[1:]   # Initialize next CCP at optimal dose from current period.
+		parms[t_s] = d_parm.value[0]
+		dyn_slacks[t_s] = h_dyn_slack.value[0]
+		health_opt[t_s + 1] = h.value[1]
 
 		# Update health for next period.
+		d_init = d.value[1:]   # Initialize next CCP at optimal dose from current period.
 		h_cur = health_prog_act_range(h_cur, t_s, t_s + 1, alpha, beta, gamma, doses, patient_rx["is_target"], health_map)[1]
 
 	# Construct full results.
 	beams_all = pad_matrix(beams, T_recov)
-	health_all = pad_matrix(h.value, T_recov)
 	doses_all = pad_matrix(doses, T_recov)
-	doses_parms = pad_matrix(d_parm.value, T_recov)
+	parms_all = pad_matrix(parms, T_recov)
 	alpha_pad = np.vstack([alpha, np.zeros((T_recov, K))])
 	beta_pad = np.vstack([beta, np.zeros((T_recov, K))])
 
-	health_est = health_prog_est(h_init, T_treat + T_recov, alpha_pad, beta_pad, gamma, doses_all, doses_parms,
+	# Extend optimal health status into recovery stage using linear-quadratic model.
+	health_recov = health_prog_act_range(health_opt[-1], T_treat - 1, T_treat + T_recov, gamma = gamma,
+										 is_target = patient_rx["is_target"], health_map = health_map)
+	health_opt_recov = np.row_stack([health_opt, health_recov[1:]])
+
+	# Compute health status from optimal doses using linearized/linear-quadratic models.
+	health_est = health_prog_est(h_init, T_treat + T_recov, alpha_pad, beta_pad, gamma, doses_all, parms_all,
 								 patient_rx["is_target"], health_map)
 	health_proj = health_prog_act(h_init, T_treat + T_recov, alpha_pad, beta_pad, gamma, doses_all, patient_rx["is_target"], health_map)
-	obj = dyn_quad_obj(d, health_proj[:(T_treat + 1)], patient_rx).value
+	obj = dyn_quad_obj(doses, health_proj[:(T_treat + 1)], patient_rx).value
 	if use_ccp_slack:
-		obj += ccp_slack_weight*np.sum(h_dyn_slack.value)
+		obj += ccp_slack_weight*np.sum(dyn_slacks)
 	# TODO: How should we handle constraint violations?
 	status, status_count = Counter(status_list).most_common(1)[0]   # Take majority as final status.
 	return {"obj": obj, "status": status, "num_iters": num_iters, "solve_time": solve_time, "beams": beams_all,
-			"doses": doses_all, "health": health_proj, "health_opt": health_all, "health_est": health_est}
+			"doses": doses_all, "health": health_proj, "health_opt": health_opt_recov, "health_est": health_est}

fractionation/utilities/data_utils.py
@@ -237,7 +237,15 @@ def check_dyn_matrices(F_list, G_list, q_list, r_list, K, T_treat, T_recov = 0):
 			raise ValueError("r_t must have dimensions ({0},)".format(K))
 	return F_list, G_list, q_list, r_list
 
-def check_quad_vectors(alpha, beta, gamma, K, T_treat, T_recov = 0):
+def check_row_range(v, v_name, K, T):
+	if len(v.shape) != 2:
+		raise ValueError("{0} must be a 2-dimensional array".format(v_name))
+	if v.shape[0] < T:
+		raise ValueError("{0} must have at least {1} rows".format(v_name,T))
+	if v.shape[1] != K:
+		raise ValueError("{0} must have exactly {1} columns".format(v_name,K))
+
+def check_quad_vectors(alpha, beta, gamma, K, T_treat, T_recov = 0, is_range = False):
 	T_total = T_treat + T_recov
 	if alpha is None:
 		alpha = np.zeros((T_treat,K))
@@ -246,18 +254,23 @@ def check_quad_vectors(alpha, beta, gamma, K, T_treat, T_recov = 0):
 	if gamma is None:
 		gamma = np.zeros((T_total,K))
 
-	if alpha.shape != (T_treat,K):
-		raise ValueError("alpha must have dimensions ({0},{1})".format(T_treat,K))
-	if beta.shape != (T_treat,K):
-		raise ValueError("beta must have dimensions ({0},{1})".format(T_treat,K))
-	if gamma.shape != (T_total,K):
-		raise ValueError("gamma must have dimensions ({0},{1})".format(T_total,K))
+	if is_range:
+		check_row_range(alpha, "alpha", K, T_treat)
+		check_row_range(beta, "beta", K, T_treat)
+		check_row_range(gamma, "gamma", K, T_total)
+	else:
+		if alpha.shape != (T_treat,K):
+			raise ValueError("alpha must have dimensions ({0},{1})".format(T_treat,K))
+		if beta.shape != (T_treat,K):
+			raise ValueError("beta must have dimensions ({0},{1})".format(T_treat,K))
+		if gamma.shape != (T_total,K):
+			raise ValueError("gamma must have dimensions ({0},{1})".format(T_total,K))
 
 	if np.any(beta < 0):
 		raise ValueError("beta can only contain nonnegative values")
 	return alpha, beta, gamma
 
-def check_prog_parms(alpha, beta, gamma, doses, K, T):
+def check_prog_parms(alpha, beta, gamma, doses, K, T, is_range = False):
 	# Defaults to no treatment.
 	if doses is None:
 		if alpha is None and beta is None:
@@ -273,12 +286,14 @@ def check_prog_parms(alpha, beta, gamma, doses, K, T):
 			beta = np.zeros((T, K))
 		elif alpha is None and beta is None:
 			raise ValueError("alpha or beta must be provided.")
-		if doses.shape != (T, K):
+		if is_range:
+			check_row_range(doses, "doses", K, T)
+		elif doses.shape != (T, K):
 			raise ValueError("doses must have dimensions ({0},{1})".format(T, K))
 	if gamma is None:
 		gamma = np.zeros((T, K))
 
-	alpha, beta, gamma = check_quad_vectors(alpha, beta, gamma, K, T, T_recov=0)
+	alpha, beta, gamma = check_quad_vectors(alpha, beta, gamma, K, T, T_recov=0, is_range=is_range)
 	return alpha, beta, gamma, doses
 
 # Health prognosis with a given treatment.
@@ -343,8 +358,10 @@ def health_prog_act(h_init, T, alpha = None, beta = None, gamma = None, doses = 
 
 def health_prog_act_range(h_init, t_s, T, alpha = None, beta = None, gamma = None, doses = None, is_target = None, health_map = lambda h,t: h):
 	K = h_init.shape[0]
-	if t_s > T:
-		raise ValueError("t_s must be less than or equal to T")
+	if t_s > T or t_s < 0:
+		raise ValueError("t_s must be an integer in [0, {0}]".format(T))
+
+	alpha, beta, gamma, doses = check_prog_parms(alpha, beta, gamma, doses, K, T, is_range = True)
 	if is_target is None:
 		is_target = np.full((K,), False)
 	if is_target.shape not in [(K,), (K, 1)]: